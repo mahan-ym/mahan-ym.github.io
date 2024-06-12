7730640eccb78a26b6cd537b50cdb8d680f2cbad
mahan_ym <mahan.yarmohammadtajari@studenti.unipd.it>
2024-06-12 14:51:44 +0200

    added persian colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '480px',
+      'md': '574px',
+      'lg': '768px',
+      'xl': '1024px',
+      '2xl': '1680px',
+    },
+    extend: {
+      colors: {
+        'persian-blue':'#1C39BB',
+        'turquoise-persian-blue':'#57C5C6',
+        'persian-indigo':'#32127A',
+        'persian-rose':'#FE28A2',
+        'persian-pink':'#F77FBE',
+        'persian-red':'#C81D11',
+        'persian-green':'#00A693',
+      },
+    },
   },
   plugins: [],
 }
