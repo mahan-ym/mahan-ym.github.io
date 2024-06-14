1b7fb8f72eb2c6616d7a9fd03cbbb85a0f067ea2
mahan_ym <mahan.yarmohammadtajari@studenti.unipd.it>
2024-06-14 20:37:14 +0200

    added experience raw component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import Home from './components/Home';
+import Home from './components/Home.jsx';
+import Experience from "./components/Experience.jsx";
 
 const App = () => {
   return (
+      <>
         <Home />
+        <Experience />
+      </>
   );
 };
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,11 @@
 function Experience() {
 
     return (
-      <>
-      </>
-    )
+      <div  className="flex items-center w-screen h-screen">
+        <h1>Experience</h1>
+        <p>Experience is the name everyone gives to their mistakes.</p>
+      </div>
+    );
   }
   
   export default Experience
