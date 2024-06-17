05197951fd6e1b41a35489db02add58a7f58e42f
mahan_ym <mahan.yarmohammadtajari@studenti.unipd.it>
2024-06-17 17:53:50 +0200

    added animated cards for experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,28 @@
+import React, { useEffect, useRef } from 'react';
+import experienceData from '../public/content/experience.json';
+import GradientCard from "./gradientCard/GradientCard.jsx";
+
+
 function Experience() {
 
-    return (
-      <div  className="flex items-center w-screen h-screen">
-        <h1>Experience</h1>
-        <p>Experience is the name everyone gives to their mistakes.</p>
-      </div>
-    );
-  }
-  
-  export default Experience
-  
+  return (
+    <div className="flex flex-col  w-screen h-screen ">
+      <h1 className="text-4xl font-bold text-white w-full m-4">Experience:</h1>
+      {experienceData.industry.map((exp, index) => (
+        <div className={index %2 === 0 ? "mr-auto m-4" : "ml-auto m-4"}>
+          <GradientCard
+              indx={index}
+              position={exp.position}
+              company={exp.company}
+              location={exp.location}
+              date={exp.date}
+              duration={exp.length}
+              description={exp.description}
+              achievements={exp.achievements} />
+          </div>
+      ))}
+    </div>
+  );
+}
+
+export default Experience;
---
+++ src/components/gradientCard/GradientCard.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import './GradientCard.css';
+
+const GradientCard = (props) => {
+    const [mousePosition, setMousePosition] = useState({x: 0, y: 0});
+    const [rotate, setRotate] = useState({ x: 0, y: 0 });
+
+    const handleMouseMove = (event) => {
+        const {clientX, clientY, currentTarget} = event;
+        const card = event.currentTarget;
+        const box = card.getBoundingClientRect();
+        const x = event.clientX - box.left;
+        const y = event.clientY - box.top;
+        const {top, left} = currentTarget.getBoundingClientRect();
+        setMousePosition({x, y});
+
+        const centerX = box.width / 2;
+        const centerY = box.height / 2;
+        const rotateX = (y - centerY) / 20;
+        const rotateY = (centerX - x) / 20;
+        setRotate({ x: rotateX, y: rotateY });
+    };
+
+    return (
+        <div
+            key={props.indx}
+            onMouseMove={handleMouseMove}
+            onMouseLeave={() => setRotate({x: 0, y: 0})}
+            className="text-container"
+            style={{
+                transform: `perspective(1000px) rotateX(${rotate.x}deg) rotateY(${rotate.y}deg) scale3d(1, 1, 1)`,
+                transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
+            }}
+        >
+            <div className="g1"/>
+            <div className="g2"/>
+            <div className="g3"/>
+            <div className="g4"/>
+            <div className="g5"/>
+            <h2>{props.position}</h2>
+            <p className={"text-2xl"}>{props.company}</p>
+            <p>{props.location}</p>
+            <p>{props.date} - {props.duration}</p>
+            <p className={"text-lg"}>{props.description}</p>
+            <ul className="list-disc list-inside">
+                {props.achievements.map((achievement, i) => (
+                    <li key={i} className="text-balance">{achievement}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default GradientCard;
