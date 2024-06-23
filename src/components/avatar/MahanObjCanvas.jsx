e690367a03ebfcdec3a7e3701a77486ccccfa0c5
mahan_ym <mahan.yarmohammadtajari@studenti.unipd.it>
2024-06-23 13:14:16 +0200

    edited lighting of the avatar

--- src/components/avatar/MahanObjCanvas.jsx
+++ src/components/avatar/MahanObjCanvas.jsx
@@ -6,9 +6,9 @@ function MahanObjCanvas() {
     return (
         <Canvas>
             <hemisphereLight color={"#ffffff"} groundColor={"#000000"} intensity={1} position={[0, 1, 0]}/>
-            <pointLight color={"#F77FBE"} intensity={100} position={[2, -4, 1]}/>
-            <pointLight color={"#57C5C6"} intensity={100} position={[-2, -4, 0]}/>
-            <pointLight color={"#F77FBE"} intensity={30} position={[2, 4, 1]}/>
+            <pointLight color={"#F77FBE"} intensity={30} position={[2, -4, 0]}/>
+            <pointLight color={"#57C5C6"} intensity={30} position={[-2, -4, 0]}/>
+            <pointLight color={"#F77FBE"} intensity={30} position={[2, 4, 0]}/>
             <pointLight color={"#57C5C6"} intensity={30} position={[-2, 4, 0]}/>
             <MahanObj/>
         </Canvas>
