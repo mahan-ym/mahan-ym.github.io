f20c3b9f1944af875d48f451090873d25a7a27d8
mahan_ym <mahan.yarmohammadtajari@studenti.unipd.it>
2024-06-26 14:19:02 +0200

    added icons to aboutme section

--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import aboutMeData from '../../public/content/aboutme.json';
 import './aboutMe.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEnvelope, faLocationDot, faPhone} from "@fortawesome/free-solid-svg-icons";
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
 
 function AboutMe() {
     const { abstract, location, email, phone, linkedIn, github } = aboutMeData;
@@ -10,11 +13,12 @@ function AboutMe() {
             <h1>About Me</h1>
             <p>{abstract}</p>
             <h2 className="mb-4 mt-4">Contact Information</h2>
-            <p>Location: {location}</p>
-            <p>Email: <a href={`mailto:${email}`}>{email}</a></p>
-            <p>Phone: {phone}</p>
-            <p>LinkedIn: <a href={linkedIn}>{linkedIn}</a></p>
-            <p>GitHub: <a href={github}>{github}</a></p>
+            <p><FontAwesomeIcon icon={faLocationDot} beat /> Location: {location}</p>
+            <p><FontAwesomeIcon icon={faEnvelope} bounce /> Email: <a href={`mailto:${email}`}>{email}</a></p>
+
+            <p><FontAwesomeIcon icon={faPhone} shake /> Phone: {phone}</p>
+            <p><FontAwesomeIcon icon={faLinkedin} fade /> LinkedIn: <a href={linkedIn}>{linkedIn}</a></p>
+            <p><FontAwesomeIcon icon={faGithub} spin /> GitHub: <a href={github}>{github}</a></p>
         </div>
     );
 }
