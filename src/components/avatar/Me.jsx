ef5c2fa67d592cd0e523fea53f2100e4c7579838
mahan_ym <mahan.yarmohammadtajari@studenti.unipd.it>
2025-04-17 03:00:07 +0200

    update role, exp, activity

--- src/components/avatar/Me.jsx
+++ src/components/avatar/Me.jsx
@@ -6,8 +6,8 @@ function Me() {
         <div className={"relative w-screen h-screen overflow-hidden"}>
             <div className="flex flex-row w-full h-full">
                 <div className="relative ml-4 xl:w-2/5 w-full h-full content-center">
-                    <h1>Mahan Y.T</h1>
-                    <h2>Software Engineer</h2>
+                    <h1>Mahan Yarmohammad</h1>
+                    <h2>AI Engineer</h2>
                 </div>
                 <div className="absolute xl:relative -z-10  xl:w-3/5 w-full h-full">
                     <MahanObjCanvas/>
