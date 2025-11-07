d5216cf389a7e4c9e498ea9dc2cdcb74d7c51cbb
mahan_ym <mahan.yarmohammadtajari@studenti.unipd.it>
2025-11-07 10:41:47 +0100

    update extension of the image

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <>
       <Suspense fallback={<div></div>}>
-        <img src="/images/back.jpg" alt="gradient" className="fixed w-full h-full -z-50 object-cover" loading="lazy" />
+        <img src="/images/back.jpeg" alt="gradient" className="fixed w-full h-full -z-50 object-cover" loading="lazy" />
       </Suspense>
       <div className="background-div"></div>
       <Me />
